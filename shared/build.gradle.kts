64c78ffb3fadd813be2c90b495c862cd30402cff
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2024-09-30 14:43:11 +0300

    Update dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.kotlinCocoapods)
-    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.cocoapods)
+    alias(libs.plugins.android.library)
 }
 
 kotlin {
